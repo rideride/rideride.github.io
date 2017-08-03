2ef14a50dfb36036b8769d8cc9fac0183964d59d
rideride <iamrideride@gmail.com>
2017-08-03 14:28:03 -0500

    Update javascripty.js

--- javascripty.js
+++ javascripty.js
@@ -387,7 +387,14 @@ if ($node.find('.body').parent().next().next().next().hasClass( "stricken" ) == 
   var colortest = '#ffff00';
   demipunoyletra = 'amazingpikachu_37';
   if (user == '/user/amazingpikachu_38') {var user2 = $node.find('.body').children().first().next().text(); $node.find('.body').append("<span id=fakeauthor></div>"); document.getElementById("fakeauthor").innerHTML = user2; $node.find('.body').children().first().next().css('fontSize', '0px'); document.getElementById("fakeauthor").style.fontSize = '13px'; document.getElementById("fakeauthor").style.color = 'transparent'; document.getElementById("fakeauthor").style.background = 'linear-gradient(to right,red,orange,yellow,green,blue,indigo,violet)'; document.getElementById("fakeauthor").style.webkitBackgroundClip = 'text'; }
-} else if (38 <= demipunoyletra && demipunoyletra < 100) {
+} else if (38 <= demipunoyletra && demipunoyletra < 47) {
+  var colortest = '#ef7070';
+} else if (47 == demipunoyletra) {
+  var colortest = '#191970';
+  var elcolor = '#ffffff';
+  demipunoyletra = '(47) kdiuro13';
+  if (user == '/user/kdiuro13') {var user2 = $node.find('.body').children().first().next().text(); $node.find('.body').append("<span id=fakeauthor></div>"); document.getElementById("fakeauthor").innerHTML = user2; $node.find('.body').children().first().next().css('fontSize', '0px'); document.getElementById("fakeauthor").style.fontSize = '13px'; document.getElementById("fakeauthor").style.color = 'transparent'; document.getElementById("fakeauthor").style.background = 'linear-gradient(to right,red,orange,yellow,green,blue,indigo,violet)'; document.getElementById("fakeauthor").style.webkitBackgroundClip = 'text'; }
+} else if (48 <= demipunoyletra && demipunoyletra < 100) {
   var colortest = '#ef7070';
 } else if (100 == demipunoyletra) {
   var colortest = '#2b0090';
