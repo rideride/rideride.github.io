fa98f2f27590561973b00570f80c28d949c3bbfe
rideride <iamrideride@gmail.com>
2018-02-08 23:17:50 -0600

    Update javascripty.js

--- javascripty.js
+++ javascripty.js
@@ -429,7 +429,13 @@ if ($node.find('.body').parent().next().next().next().hasClass( "stricken" ) == 
       var elcolor = '#ffffff';
       if (user == '/user/dominodan123') {var user2 = $node.find('.body').children().first().next().text(); $node.find('.body').append("<span id=fakeauthor></div>"); document.getElementById("fakeauthor").innerHTML = user2; $node.find('.body').children().first().next().css('fontSize', '0px'); document.getElementById("fakeauthor").style.cssText = 'font-size: 13px; color: transparent; background: linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet); -webkit-background-clip: text!important;';}
     }
-} else if (124 <= demipunoyletra && demipunoyletra < 200) {
+} else if (124 <= demipunoyletra && demipunoyletra < 151) {
+  var colortest = '#ffaeae';
+} else if (151 == demipunoyletra) {
+  var colortest = '#FFFF33';
+  demipunoyletra = '(151) MewDP';
+  if (user == '/user/MewDP') {var user2 = $node.find('.body').children().first().next().text(); $node.find('.body').append("<span id=fakeauthor></div>"); document.getElementById("fakeauthor").innerHTML = user2; $node.find('.body').children().first().next().css('fontSize', '0px'); document.getElementById("fakeauthor").style.cssText = 'font-size: 13px; color: transparent; background: linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet); -webkit-background-clip: text!important;';}
+} else if (152 <= demipunoyletra && demipunoyletra < 200) {
   var colortest = '#ffaeae';
 } else if (200 <= demipunoyletra && demipunoyletra < 220) {
   var colortest = '#ffebba';
