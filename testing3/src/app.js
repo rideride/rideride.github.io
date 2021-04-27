e59898d314a7ade2ce61ad729196bb8cf3107c1b
rideride <iamrideride@gmail.com>
2021-04-26 23:26:28 -0500

    additional testing,

--- testing3/src/app.js
+++ testing3/src/app.js
@@ -2,7 +2,7 @@
 
 document.body.appendChild(component());*/
 
-console.log('starting test.js (yeah)');
+console.log('starting test.js (yeah3)');
 
 
 const snoowrap = require('snoowrap');
@@ -15,11 +15,31 @@ var authenticationUrl = snoowrap.getAuthUrl({
     state: 'fe211bebc52eb3da9bef8db6e63104d3' // a random string, this could be validated when the user is redirected back
   });
 
+  window.location = authenticationUrl;
+
+  const code = new URL(window.location.href).searchParams.get('code');
+
+  function createInstance(token) {
+    return new snoowrap({
+      userAgent:"testing3",
+      clientId:"LonIowH9Qe2RzA",
+      clientSecret:"",
+      refreshToken:token,
+    })
+  }
+
+  const r = createInstance(code);
+console.log(r.getHot());
+
+  //window.location = authenticationUrl;
+
+/*
 const r = new snoowrap({
     client_id: 'LonIowH9Qe2RzA',
     client_secret: '',
   });
 
 r.get_livethread('whrdxo8dg9n0').stream.on('update', console.log);
+*/
 
 console.log('ending test.js');
