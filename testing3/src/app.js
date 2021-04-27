f62333aee976467c91ca1c59cb0ea0bca1288c23
rideride <iamrideride@gmail.com>
2021-04-27 00:46:12 -0500

    bruh i didnt know read wasnt a default scope lol

--- testing3/src/app.js
+++ testing3/src/app.js
@@ -2,7 +2,7 @@
 
 document.body.appendChild(component());*/
 
-console.log('starting test.js (yeah9)');
+console.log('starting test.js (yeah10)');
 
 
 const snoowrap = require('snoowrap');
@@ -12,7 +12,7 @@ function createAuth1() {
     console.log("creating auth")
 var authenticationUrl = snoowrap.getAuthUrl({
     clientId: 'LonIowH9Qe2RzA',
-    scope: ['identity', 'livemanage', 'edit'],
+    scope: ['identity', 'livemanage', 'edit', 'read'],
     redirectUri: 'https://rideride.github.io/testing3/deploy/index.html',
     permanent: true,
     state: 'fe211bebc52eb3da9bef8db6e63104d3' // a random string, this could be validated when the user is redirected back
